58607e2917841b53bb16bc74345b3fa3dc9e6482
Zoltan, Kleri <kleriz@gmail.com>
2019-03-14 18:05:18 +0100

    Create constructor, getters, setters

--- src/amobaPractice/Board.java
+++ src/amobaPractice/Board.java
@@ -2,4 +2,30 @@ package amobaPractice;
 
 public class Board {
 
+	public int sizeX;
+	public int sizeY;
+	
+
+	public Board(int sizeX, int sizeY) {
+		super();
+		this.sizeX = sizeX;
+		this.sizeY = sizeY;
+	}
+	
+	
+	public int getSizeX() {
+		return sizeX;
+	}
+	public void setSizeX(int sizeX) {
+		this.sizeX = sizeX;
+	}
+	public int getSizeY() {
+		return sizeY;
+	}
+	public void setSizeY(int sizeY) {
+		this.sizeY = sizeY;
+	}
+	
+	
+	
 }
